9ee4be97365c5230acd4582030fd594c815e87d5
JochiPochi <john.aleman@cyphyworks.com>
2016-06-18 14:40:43 -0400

    spawn: add commands

--- spawn-root.sh
+++ spawn-root.sh
@@ -7,7 +7,15 @@ echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt
 apt-get update
 apt-get --allow-unauthenticated install sur5r-keyring
 apt-get update
-apt-get install i3
+
+#i3 stuff
+apt-get install i3 feh
+
+#git stuff
+apt-get install gitk git-gui
+
+#other stuff
+vim ssh synaptic
 
 
 
--- spawn-user.sh
+++ spawn-user.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 #This portion of the scropt should be run as a user
-
-
-
+mkdir ~/.i3
+cp ./i3/.fehbg ~/.i3/
+cp ./i3/wallpz.jpg ~/.i3/
+cp ./i3/config ~/.i3/
+cp ./i3/.i3status.conf ~/
+cp ./.gitcolors.sh ~/
+cp ./ywtricks.txt ~/
+mkdir -p ~/.vim/plugin
+echo "source ~/.gitcolors" >> ~/.bashrc
 
 
 gsettings set org.gnome.desktop.background show-desktop-icons false
