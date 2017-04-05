06c138030fdb250d9736fa402a29acb86216bde3
JochiPochi <john.aleman@cyphyworks.com>
2017-04-05 12:43:29 -0400

    ywtricks: Add more useful commands

--- ywtricks.sh
+++ ywtricks.sh
@@ -5,6 +5,8 @@ nmcli nm wifi off
 #Rescan PCI ports
 echo 1 | sudo tee /sys/bus/pci/rescan
 
+#Volume
+amixer -D pulse sset Master 5%+
 
 #Fix Chrome dependency error:
 
@@ -13,11 +15,13 @@ sudo gedit /etc/apt/sources.list.d/google-chrome.list
 deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
 
 
-#Finding things
+#Finding and replace things
 
 find FOLDER_ADDRESS -name "*something*"
 
-grep -rnw '/path/to/somewhere/' -e "pattern"
+grep -rnw './' -e "pattern"
+
+grep -rl 'string_to_find' ./ | xargs sed -i 's:string_to_replace:string_to_replace_with:g'
 
 #Going to previous folder on shell
 cd -
@@ -46,26 +50,72 @@ Press $mod+Right to focus Window Two (command focus right)
 Press $mod+Shift+Left to move Window Two left, which will put it under Window One (command move left)
 
 
-ZIP
+### ZIP
 
-# zip -r archive_name.zip directory_to_compress
+zip -r archive_name.zip directory_to_compress
 
-# unzip archive_name.zip
+unzip archive_name.zip
 
-TAR
+### TAR
 
-# tar -cvf archive_name.tar directory_to_compress
+tar -cvf archive_name.tar directory_to_compress
 
- tar -xvf archive_name.tar.gz
+tar -xvf archive_name.tar.gz
 
-TAR.GZ
+### TAR.GZ
 
-# tar -zcvf archive_name.tar.gz directory_to_compress
+tar -zcvf archive_name.tar.gz directory_to_compress
 
-# tar -zxvf archive_name.tar.gz
+tar -zxvf archive_name.tar.gz
 
-TAR.BZ2
+### TAR.BZ2
 
-# tar -jcvf archive_name.tar.bz2 directory_to_compress
+tar -jcvf archive_name.tar.bz2 directory_to_compress
 
-# tar -jxvf archive_name.tar.bz2 -C /tmp/extract_here/
+tar -jxvf archive_name.tar.bz2 -C /tmp/extract_here/
+
+### TAR.XZ
+
+tar xvfJ filename.tar.xz
+
+
+### Bitbake recipe example
+
+DESCRIPTION = "cameracapture application" 
+SECTION = "examples" 
+LICENSE = "CLOSED" 
+PR = "r0" 
+
+DEPENDS = "opencv"
+
+SRC_URI = "git://github.com/zafrullahsyed/cameracapture.git;protocol=https;tag=v0.1"
+
+S = "${WORKDIR}/git"
+
+inherit pkgconfig cmake
+
+do_install() {
+    install -d ${D}${bindir}
+    install -m 0755 cameracapture ${D}${bindir}
+}
+
+#Monitor enable
+xrandr --current
+xrandr --output VGA1 --off
+xrandr --output VGA1 --auto --right-of LVDS1
+
+#Check out system logs
+sudo tail -f /var/log/syslog
+
+#Check user systemd units
+systemctl --user --all | grep "cyphy"
+
+#Look at unit logs:
+journalctl --user-unit unit-name
+systemd-analyze plot > techtransit.svg
+
+#Check Ubuntu Version
+lsb_release -r | awk '{print $2}'
+
+#Install SSH Key on server
+ssh-copy-id -i ~/.ssh/id_rsa.pub remote-host
