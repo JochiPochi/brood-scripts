1b3240bc8f1ab4cf04393e526b5cecf1b7daeb34
JochiPochi <john.aleman@cyphyworks.com>
2017-08-30 19:06:38 -0400

    root script: Dont remove unity by default

--- spawn-root.sh
+++ spawn-root.sh
@@ -11,6 +11,13 @@ apt-get update
 #i3 stuff
 apt-get install i3 feh
 
+#remove unity
+#apt-get autoremove --purge compiz compiz-gnome compiz-plugins-default libcompizconfig0
+
+#autoremove --purge unity unity-common unity-services unity-lens-* unity-scope-* libunity-core-6* libunity-misc4 appmenu-gtk appmenu-gtk3 appmenu-qt* overlay-scrollbar* activity-log-manager-control-center firefox-globalmenu thunderbird-globalmenu
+# the following command will disable the desktop (we won't need it with i3!)
+gsettings set org.gnome.desktop.background show-desktop-icons false
+
 #git stuff
 apt-get install gitk git-gui
 
