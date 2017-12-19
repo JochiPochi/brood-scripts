ff332b0251b7e5f204bd7007f069f65b17f22e08
JochiPochi <john.aleman@cyphyworks.com>
2017-12-19 02:39:50 -0500

    root-script: Re-factor root script.
    Easier to read now. And it works better with Ubuntu 16.04

--- spawn-root.sh
+++ spawn-root.sh
@@ -1,28 +1,53 @@
 #!/bin/sh
 
-#This portion of the scropt should be run as root
+#This script should be run as root
 
-echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
 
+#Install i3
+/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
+dpkg -i ./keyring.deb
+echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
 apt-get update
-apt-get --allow-unauthenticated install sur5r-keyring
-apt-get update
+apt-get install -y i3
 
-#i3 stuff
-apt-get install i3 feh
+#Install Build tools
+apt-get install -y build-essential
 
-#remove unity
-#apt-get autoremove --purge compiz compiz-gnome compiz-plugins-default libcompizconfig0
+#Install i3 musts
+apt-get install -y feh arandr xbacklight
 
-#autoremove --purge unity unity-common unity-services unity-lens-* unity-scope-* libunity-core-6* libunity-misc4 appmenu-gtk appmenu-gtk3 appmenu-qt* overlay-scrollbar* activity-log-manager-control-center firefox-globalmenu thunderbird-globalmenu
-# the following command will disable the desktop (we won't need it with i3!)
+# The following command will disable the desktop (we won't need it with i3!)
 gsettings set org.gnome.desktop.background show-desktop-icons false
 
-#git stuff
-apt-get install gitk git-gui
+#Install git stuff
+apt-get -y install gitk git-gui
 
-#other stuff
-vim ssh synaptic
+#Install oher useful dev apps
+apt-get install -y vim ssh synaptic tree baobab tmux gparted
 
+#exFAT support
+apt-get install exfat-fuse exfat-utils
+
+#Install silly stuff
+apt-get install -y cmatrix
+
+#Install VM stuff
+echo "deb http://download.virtualbox.org/virtualbox/debian xenial contrib" >> /etc/apt/sources.list
+wget https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+apt-get update
+apt-get install -y virtualbox-5.1
+apt-get install -y dkms
+
+#Install Ansible
+apt-get -y install software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get -y install ansible
+
+#Install slack
+export SLACK_VERSION=2.7.1
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-${SLACK_VERSION}-amd64.deb
+dpkg -i slack-desktop-${SLACK_VERSION}-amd64.deb
+rm slack-desktop-${SLACK_VERSION}-amd64.deb
 
 
