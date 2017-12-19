114c9da6eca194a9f07ab8b97767510e9a9aba35
JochiPochi <john.aleman@cyphyworks.com>
2017-12-19 02:37:32 -0500

    user-script: Add jetbrains stuff

--- spawn-user.sh
+++ spawn-user.sh
@@ -14,7 +14,17 @@ echo "source ~/.gitcolors.sh" >> ~/.bashrc
 
 gsettings set org.gnome.desktop.background show-desktop-icons false
 
-
 #Settings
 git config --global user.email "john.aleman@cyphyworks.com"
 git config --global user.name "JochiPochi"
+
+#User level Dev Tools
+PYCHARM_VERSION=2017.2.2
+cd ~/
+mkdir bin
+wget https://download.jetbrains.com/python/pycharm-community-$PYCHARM_VERSION.tar.gz
+tar -zxf pycharm-community-$PYCHARM_VERSION.tar.gz
+rm pycharm-community-$PYCHARM_VERSION.tar.gz
+ln -s pycharm-community-2017.2.2/bin/pycharm.sh pycharm
+
+
