0a4c1712a5618bf0f7102ad3c52c4faef3f621a6
JochiPochi <aleman@mit.edu>
2018-10-23 18:15:20 -0400

    spawn-user: Fix minor bash issues

--- spawn-user.sh
+++ spawn-user.sh
@@ -2,13 +2,13 @@
 
 #This portion of the scropt should be run as a user
 mkdir ~/.i3
-ln -s  ./i3/i3status.conf ~/.i3status.conf 
+ln -s  $PWD/i3/i3status.conf ~/.i3status.conf 
 cp ./.gitcolors.sh ~/
 
 mkdir ~/.config/terminator
 cp terminator/config ~/.config/terminator/config
 
-ln -s ./ywtricks.txt ~/
+ln -s $PWD/ywtricks.txt ~/
 mkdir -p ~/.vim/plugin
 cp ./vim/Crystallite.vim ~/.vim/plugin/
 echo "source ~/.gitcolors.sh" >> ~/.bashrc
@@ -23,9 +23,10 @@ git config --global user.name "JochiPochi"
 PYCHARM_VERSION=2018.2.4
 cd ~/
 mkdir bin
+cd bin
 wget https://download.jetbrains.com/python/pycharm-community-$PYCHARM_VERSION.tar.gz
 tar -zxf pycharm-community-$PYCHARM_VERSION.tar.gz
 rm pycharm-community-$PYCHARM_VERSION.tar.gz
-ln -s pycharm-community-2017.2.2/bin/pycharm.sh pycharm
+ln -s pycharm-community-$PYCHARM_VERSION/bin/pycharm.sh pycharm
 
 
